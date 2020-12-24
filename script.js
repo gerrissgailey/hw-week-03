9ac931160750ec3ab86890c3909895b783d04740
Gerriss Gailey <gerrissgailey@gmail.com>
2020-12-23 20:51:53 -0700

    Completed JavaScript

--- script.js
+++ script.js
@@ -1,83 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Password criteria
 
+// User inputs
+var passLength = [];
 var criteriaArr = [];
-var specialChar = "!()?[]`;:@#$%^&";
-var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-var upperChar = lowerChar.toUpperCase();
-var numberChar = "1234567890";
+
+// Password criteria
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberChar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialChar = ["!", "(", ")", "?", "[", "]", "`", ";", ":", "@", "#", "$", "%", "^", "&"];
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Initialize password generation - how many characters?
-function generatePassword() {
-  var passLength = prompt("How long do you want your password to be? (Enter between 8 and 128 characters)");
-  if (passLength === null) {
-    alert("FAILED");
-    // clearTimeout;
-  }
-  else if (passLength >= 8 && passLength <= 128) {
-    criteriaArr.push(passLength);
-    criteriaSelect();
-  }
-  else if (!(passLength >= 8 && passLength <= 128)) {
-    alert("You didn't enter a number between 8 and 128.");
-    generatePassword();
-  }
-  // How do I get the alert window's "Cancel" to trigger the else?
-}
-
-// Choose password character types
-function criteriaSelect() {
-  var incSpecial = confirm("Should this password include special characters?");
-  var incUpper = confirm("Should this password include upper case letters?");
-  var incNumber = confirm("Should this password include numbers?");
-  // Why don't the below "if" statements push "false" data from the above variables into criteriaArr??
-  if (incSpecial) {
-    criteriaArr.push(true);
-  }
-  if (!(incSpecial)) {
-    criteriaArr.push(false);
-  }
-  if (incUpper) {
-    criteriaArr.push(true);
-  }
-  if (!(incUpper)) {
-    criteriaArr.push(false);
-  }
-  if (incNumber) {
-    criteriaArr.push(true);
-  }
-  if (!(incNumber)) {
-    criteriaArr.push(false);
-  }
-  verifyCriteria();
-}
-
-  
-// Veryfy password criteria selections
-function verifyCriteria() {
-  var ver = criteriaArr.includes(true);
-  if (ver) {
-    // Figure out steps to take if true... Run randomizer function??
-  }
-  else if (!(ver)) {
-  var warn = confirm("WARNING! You did not include any special, upper case, or numerical characters. Excluding these will produce a weak password using only lower case letters. Click OK to proceed or click Cancel to start over.")
-  // Nest another if else Here...
-    if (warn) {
-      alert("ok clicked");
-    }
-    else {
-      alert("cancel clicked");
-      criteriaArr = [];
-    }
-  }
-}
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -85,12 +22,84 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+// Initialize password generation - how many characters?
+function generatePassword() {
+  passLength = prompt("How long do you want your password to be? (Enter between 8 and 128 characters)");
+  // If user clicks cancel, then abort the generatePassword function
+  if (passLength === null) {
+    return alert("FAILED");
+    // clearTimeout;
+  }
+  // If user enters incorrect length...
+  else if (!(passLength >= 8 && passLength <= 128)) {
+    alert("You didn't enter a number between 8 and 128.");
+    return generatePassword();
+  }
+
+  // Choose password character type.
+    var incLower = confirm("Should this password include lower case letters");
+    var incUpper = confirm("Should this password include upper case letters?");
+    var incNumber = confirm("Should this password include numbers?");
+    var incSpecial = confirm("Should this password include special characters?");
+  
+
+  // Only run password generator if user selects options from below criteria to use.
+  if (incLower) {
+    criteriaArr=criteriaArr.concat(lowerChar);
+  }
+  if (incUpper) {
+    criteriaArr=criteriaArr.concat(upperChar);
+  }
+  if (incNumber) {
+    criteriaArr=criteriaArr.concat(numberChar);
+  }
+  if (incSpecial) {
+    criteriaArr=criteriaArr.concat(specialChar);
+  }
+  if (!incLower && !incUpper && !incNumber && !incSpecial) {
+    alert("You must choose at least one criteria!");
+    return generatePassword();
+  };
+
+  var collectPass = "";
+  console.log(criteriaArr);
+  //Randomly choose password characters based on the criteria selected.
+  for (var i = 0; i < passLength; i++) {
+    var randomChars = criteriaArr[Math.floor(Math.random() * criteriaArr.length)];
+    collectPass+=randomChars;
+  }
+  
+  return collectPass
+}
+
+// Variable to push the randomized password characters to
 
 
 
+
+//   verifyCriteria();
+// var ver = []
+
+// // Veryfy password criteria selections
+// function verifyCriteria() {
+//   var ver = criteriaArr.includes(true);
+//   if (ver) {
+//     // Figure out steps to take if true... Run randomizer function??
+//   }
+//   else if (!(ver)) {
+//     // var warn = confirm("WARNING! You did not include any special, upper case, or numerical characters. Excluding these will produce a weak password using only lower case letters. Click OK to proceed or click Cancel to start over.")
+//     // Nest another if else Here...
+//     if (warn) {
+//       alert("ok clicked");
+//     }
+//     else {
+//       alert("cancel clicked");
+//       criteriaArr = [];
+//     }
+//   }
+// }
 
 
 
